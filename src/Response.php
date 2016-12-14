76e80915b3681f6baa43abe0889ac43ad2431509
houdunwang <2300071698@qq.com>
2016-12-14 17:43:11 +0800

    优化

--- src/Response.php
+++ src/Response.php
@@ -7,7 +7,7 @@
  * |    WeChat: aihoudun
  * | Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
  * '-------------------------------------------------------------------*/
-namespace hdphp\component;
+namespace houdunwang\response;
 
 class Response {
 	/**
