90bd73fcfcb880ecaa5fc9e0ef53cf30c4e164ac
houdunwang <2300071698@qq.com>
2016-12-28 14:54:50 +0800

    no message

--- src/Response.php
+++ src/Response.php
@@ -1,33 +1,34 @@
-<?php
-/** .-------------------------------------------------------------------
- * |  Software: [HDCMS framework]
- * |      Site: www.hdcms.com
- * |-------------------------------------------------------------------
- * |    Author: 向军 <2300071698@qq.com>
- * |    WeChat: aihoudun
- * | Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
- * '-------------------------------------------------------------------*/
-namespace houdunwang\response;
+<?php namespace houdunwang\response;
+	/** .-------------------------------------------------------------------
+	 * |  Software: [HDCMS framework]
+	 * |      Site: www.hdcms.com
+	 * |-------------------------------------------------------------------
+	 * |    Author: 向军 <2300071698@qq.com>
+	 * |    WeChat: aihoudun
+	 * | Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
+	 * '-------------------------------------------------------------------*/
 
 //请求处理
-
 use houdunwang\response\build\Base;
 
 class Response {
-	protected static function link() {
-		static $link = null;
-		if ( is_null( $link ) ) {
-			$link = new Base();
-		}
+	protected $link;
 
-		return $link;
+	protected function driver() {
+		$this->link = new Base();
+
+		return $this;
 	}
 
 	public function __call( $method, $params ) {
-		return call_user_func_array( [ self::link(), $method ], $params );
+		if ( is_null( $this->link ) ) {
+			$this->driver();
+		}
+
+		return call_user_func_array( [ $this->link, $method ], $params );
 	}
 
 	public static function __callStatic( $name, $arguments ) {
-		return call_user_func_array( [ self::link(), $name ], $arguments );
+		return call_user_func_array( [ new static(), $name ], $arguments );
 	}
 }
