4addbe5c714e08719f4913b22e5b5f9459a83dda
houdunwang <2300071698@qq.com>
2017-04-23 19:57:46 +0800

    no message

--- src/ResponseProvider.php
+++ src/ResponseProvider.php
@@ -7,20 +7,27 @@
  * |    WeChat: aihoudun
  * | Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
  * '-------------------------------------------------------------------*/
+
 namespace houdunwang\response;
 
 use houdunwang\framework\build\Provider;
 
-class ResponseProvider extends Provider {
-	//延迟加载
-	public $defer = true;
+class ResponseProvider extends Provider
+{
+    //延迟加载
+    public $defer = true;
 
-	public function boot() {
-	}
+    public function boot()
+    {
+    }
 
-	public function register() {
-		$this->app->single( 'Response', function () {
-			return new Response();
-		} );
-	}
+    public function register()
+    {
+        $this->app->single(
+            'Response',
+            function () {
+                return new Response();
+            }
+        );
+    }
 }
